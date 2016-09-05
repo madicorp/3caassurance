984eb4c3b9f559dee4a0b0602466eeb9c57c0878
ekougs <ekougs@gmail.com>
2016-09-05 21:54:26 +0000

    release_1_delivery: update wsgi.py and prod settings

--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,10 @@
-"""
-WSGI config for _3caassurance project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
-
-from __future__ import absolute_import, unicode_literals
-
+#!/usr/bin/python
 import os
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "_3caassurance.settings.prod")
-
-application = get_wsgi_application()
+from _3caassurance.wsgi import application
