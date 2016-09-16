6f1d35b73c5333f916f696ead7655185c75c5292
ekougs <ekougs@gmail.com>
2016-09-16 10:38:38 +0000

    release_3_to_1.0: fix static assets dirs

--- _3caassurance/settings/prod.py
+++ _3caassurance/settings/prod.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from .base import *
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['3caassurance-tiays.rhcloud.com']
 
 SECRET_KEY = os.environ['OPENSHIFT_SECRET_TOKEN']
@@ -14,7 +14,7 @@ except ImportError:
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'static')
+STATIC_ROOT = os.path.join(os.environ['OPENSHIFT_REPO_DIR'], '_3caassurance', 'static')
 MEDIA_ROOT = os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'media')
-STATICFILES_DIRS = (os.path.join(os.environ['OPENSHIFT_REPO_DIR'], '_3caassurance', 'static'),)
+STATICFILES_DIRS = (os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'static'),)
 TEMPLATE_DIRS = (os.path.join(os.environ['OPENSHIFT_REPO_DIR'], '_3caassurance', 'templates'),)
