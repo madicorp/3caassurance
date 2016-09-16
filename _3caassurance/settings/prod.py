f418ea21491870ed26ce3b8f7c097f473a523046
ekougs <ekougs@gmail.com>
2016-09-16 11:18:02 +0000

    release_3_to_1.0: fix static assets dirs

--- _3caassurance/settings/prod.py
+++ _3caassurance/settings/prod.py
@@ -14,7 +14,7 @@ except ImportError:
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(os.environ['OPENSHIFT_REPO_DIR'], '_3caassurance', 'static')
+STATIC_ROOT = os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'static')
 MEDIA_ROOT = os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'media')
-STATICFILES_DIRS = (os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'static'),)
+STATICFILES_DIRS = (os.path.join(os.environ['OPENSHIFT_REPO_DIR'], '_3caassurance', 'static'),)
 TEMPLATE_DIRS = (os.path.join(os.environ['OPENSHIFT_REPO_DIR'], '_3caassurance', 'templates'),)
