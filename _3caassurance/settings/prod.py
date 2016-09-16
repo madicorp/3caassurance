f5d9a97dae471668d00590ab614c34bce0b61f88
ekougs <ekougs@gmail.com>
2016-09-16 11:28:31 +0000

    release_3_to_1.0: use base static and media files locations

--- _3caassurance/settings/prod.py
+++ _3caassurance/settings/prod.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from .base import *
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['3caassurance-tiays.rhcloud.com']
 
 SECRET_KEY = os.environ['OPENSHIFT_SECRET_TOKEN']
@@ -11,10 +11,3 @@ try:
     from .local import *
 except ImportError:
     pass
-
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'static')
-MEDIA_ROOT = os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'media')
-STATICFILES_DIRS = [os.path.join(os.environ['OPENSHIFT_REPO_DIR'], '_3caassurance', 'static'),]
-TEMPLATE_DIRS = (os.path.join(os.environ['OPENSHIFT_REPO_DIR'], '_3caassurance', 'templates'),)
