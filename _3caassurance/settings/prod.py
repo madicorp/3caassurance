fd5531c5b978128b0080a26e611f63100d91f990
ekougs <ekougs@gmail.com>
2016-09-16 12:00:08 +0000

    release_3_to_1.0: static and media handled by openshift local django server

--- _3caassurance/settings/prod.py
+++ _3caassurance/settings/prod.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from .base import *
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['3caassurance-tiays.rhcloud.com']
 
 SECRET_KEY = os.environ['OPENSHIFT_SECRET_TOKEN']
@@ -12,9 +12,11 @@ try:
 except ImportError:
     pass
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+OPENSHIFT_PYTHON_URL = 'http://' + os.environ['OPENSHIFT_PYTHON_IP'] + ':' + os.environ['OPENSHIFT_PYTHON_PORT']
+
+STATIC_URL = OPENSHIFT_PYTHON_URL + '/static/'
+MEDIA_URL = OPENSHIFT_PYTHON_URL + '/media/'
 STATIC_ROOT = os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'static')
-MEDIA_ROOT = os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'media')
-STATICFILES_DIRS = [os.path.join(os.environ['OPENSHIFT_REPO_DIR'], '_3caassurance', 'static'),]
+MEDIA_ROOT = os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'media')
+STATICFILES_DIRS = [os.path.join(os.environ['OPENSHIFT_REPO_DIR'], '_3caassurance', 'static'), ]
 TEMPLATE_DIRS = (os.path.join(os.environ['OPENSHIFT_REPO_DIR'], '_3caassurance', 'templates'),)
