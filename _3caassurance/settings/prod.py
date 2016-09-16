d6a429d4165cb9a314c2f9e8d27c283b342ffe79
ekougs <ekougs@gmail.com>
2016-09-16 12:24:39 +0000

    release_3_to_1.0: static and media fix

--- _3caassurance/settings/prod.py
+++ _3caassurance/settings/prod.py
@@ -7,16 +7,35 @@ ALLOWED_HOSTS = ['3caassurance-tiays.rhcloud.com']
 
 SECRET_KEY = os.environ['OPENSHIFT_SECRET_TOKEN']
 
+BASE_DIR = os.environ['OPENSHIFT_REPO_DIR']
+PROJECT_DIR = os.path.join(BASE_DIR, '_3caassurance')
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [os.path.join(PROJECT_DIR, 'static')]
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(PROJECT_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django_settings_export.settings_export',
+            ],
+        },
+    },
+]
+
 try:
     from .local import *
 except ImportError:
     pass
-
-OPENSHIFT_PYTHON_URL = 'http://' + os.environ['OPENSHIFT_PYTHON_IP'] + ':' + os.environ['OPENSHIFT_PYTHON_PORT']
-
-STATIC_URL = OPENSHIFT_PYTHON_URL + '/static/'
-MEDIA_URL = OPENSHIFT_PYTHON_URL + '/media/'
-STATIC_ROOT = os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'static')
-MEDIA_ROOT = os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'media')
-STATICFILES_DIRS = [os.path.join(os.environ['OPENSHIFT_REPO_DIR'], '_3caassurance', 'static'), ]
-TEMPLATE_DIRS = (os.path.join(os.environ['OPENSHIFT_REPO_DIR'], '_3caassurance', 'templates'),)
