0d5b47aea1b901593bd19bac08b8a2df066df1a5
ekougs <ekougs@gmail.com>
2016-09-27 00:51:04 +0000

    release_3_to_1.0: fix settings ; wait for services needed for startup

--- _3caassurance/settings/local.py
+++ _3caassurance/settings/local.py
@@ -8,8 +8,3 @@ DEBUG = True
 SECRET_KEY = '3ysrfps@vh1b1#g@k@rw6(%s=jv9%4ghp=8it_ydj&g6gn_268'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-try:
-    from .local import *
-except ImportError:
-    pass
--- docker/3ca/launch.sh
+++ docker/3ca/launch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+/usr/src/3ca/docker/3ca/wait-for-it.sh "$POSTGRES_1_PORT_5432_TCP_ADDR:$POSTGRES_1_PORT_5432_TCP_PORT" && \
 python manage.py migrate --noinput && python manage.py loaddata user images _3caassurance && \
 python manage.py update_index && python manage.py runserver 0.0.0.0:8000
