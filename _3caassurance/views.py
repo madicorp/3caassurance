0c2802e6b72705571d0fb76e2d346f069e16bf6f
ekougs <ekougs@gmail.com>
2016-09-30 00:32:06 +0000

    release_4_contact: i18ned confirmation message sent to user

--- _3caassurance/views.py
+++ _3caassurance/views.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from __future__ import absolute_import, unicode_literals
 
 import uuid
@@ -8,6 +9,7 @@ from django.core.mail.message import BadHeaderError, EmailMessage
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
+from django.utils.translation import ugettext_lazy
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -44,23 +46,44 @@ def search(request):
     })
 
 
+def __send_contact_email(user_email, user_name, user_message, ref_num, connection):
+    ref_str = '#%s' % ref_num
+    contact_mail_ref = '%s : Contact de %s' % (ref_str, user_name)
+    EmailMessage(subject=contact_mail_ref, from_email=settings.CONTACT_EMAIL,
+                 body=user_message, to=[settings.CONTACT_EMAIL],
+                 reply_to=[user_email], headers={'Sender': user_email},
+                 connection=connection).send()
+
+
+def __send_confirmation_email(user_email, user_name, user_message, ref_num, connection):
+    confirmation_mail_ref = ugettext_lazy('#%s : Inquiry reception confirmation') % ref_num
+    confirmation_mail_body_p1 = \
+        ugettext_lazy('Dear %s,\n\nWe received your inquiry #%s and will answer soon.\n') % \
+        (user_name, ref_num)
+    confirmation_mail_body_p2 = \
+        ugettext_lazy('Your inquiry:\n\n%s\n\nSincerely,\n\n3CA Team') % user_message
+    EmailMessage(subject=confirmation_mail_ref, from_email=settings.CONTACT_EMAIL,
+                 body=(confirmation_mail_body_p1 + confirmation_mail_body_p2), to=[user_email],
+                 connection=connection).send()
+
+
 @api_view(['POST'])
 def post_message(request):
     if 'POST' == request.method:
         data = request.data
         try:
             with mail.get_connection() as connection:
-                mail_ref = 'REF #%s: Contact de %s' % (uuid.uuid4(), data['contact_name'])
+                ref_num = uuid.uuid4()
+                user_name = data['contact_name']
+                user_message = data['message']
                 user_email = data['contact_email']
-                EmailMessage(subject=mail_ref, from_email=settings.CONTACT_EMAIL, body=data['message'],
-                             to=[settings.CONTACT_EMAIL],
-                             reply_to=[user_email], headers={'Sender': user_email},
-                             connection=connection).send()
+                __send_contact_email(user_email, user_name, user_message, ref_num, connection)
+                __send_confirmation_email(user_email, user_name, user_message, ref_num, connection)
         except BadHeaderError:
             # Headers injection prevention
             # https://docs.djangoproject.com/fr/1.10/topics/email/#preventing-header-injection
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        return Response(status=status.HTTP_202_ACCEPTED)
+        return Response(status=status.HTTP_200_OK)
 
 
 def handler404(request):
